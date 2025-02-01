29a350412e026515ff707f2db162aec67140e741
Mohd Anash khan <113244395+khananask908@users.noreply.github.com>
2025-02-02 00:11:16 +0530

    Update Fibbonacci.cpp
    no code wass there so i added it

--- Lecture032 Recursion Day2/Fibbonacci.cpp
+++ Lecture032 Recursion Day2/Fibbonacci.cpp
@@ -1,11 +1,15 @@
-#include<iostream> 
-using namespace std;
+class Solution {
+public:
+    int fib(int n) {
+       if (n==0)
+       return 0;
+       if(n==1)
+       return 1 ; 
 
+       int ans =fib(n-1)+fib(n-2);
 
-int main() {
-
-
-
-
-    return 0;
+       return ans ; 
+        
+    }
+};
 }

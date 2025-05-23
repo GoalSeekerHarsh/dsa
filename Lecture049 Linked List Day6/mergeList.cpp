7e35dc05fa63adbf143d27790f7353b62b4f87bb
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2025-05-23 11:27:41 +0530

    Completed the solution for L49 - merge two sorted lists

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -1,7 +1,7 @@
 /************************************************************
 
     Following is the linked list node structure.
-    
+
     template <typename T>
     class Node {
         public:
@@ -21,51 +21,60 @@
     };
 
 ************************************************************/
+Node<int> *solve(Node<int> *first, Node<int> *second)
+{
+    if (first->next == NULL)
+    {
+        first->next = second;
+        return first;
+    }
 
-void solve(Node<int>* first, Node<int>* second) {
-    
-    
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
-    
-    while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
-            
-            curr1 -> next = curr2;
-            curr2 -> next = next1;
+    Node<int> *curr1 = first;
+    Node<int> *next1 = curr1->next;
+    Node<int> *curr2 = second;
+    Node<int> *next2 = second->next;
+
+    while (next1 != NULL && curr2 != NULL)
+    {
+        if ((curr2->data >= curr1->data) && (curr2->data <= next1->data))
+        {
+
+            curr1->next = curr2;
+            next2 = curr2->next;
+            curr2->next = next1;
+
             curr1 = curr2;
             curr2 = next2;
         }
-        else {
-            
+        else
+        {
+            curr1 = next1;
+            next1 = next1->next;
+
+            if (next1 == NULL)
+            {
+                curr1->next = curr2;
+                return first;
+            }
         }
-        
-        
     }
-    
-    
+    return first;
 }
 
-Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
+Node<int> *sortTwoLists(Node<int> *first, Node<int> *second)
 {
-    if(first == NULL)
+    if (first == NULL)
         return second;
-    
-    if(second == NULL)
+
+    if (second == NULL)
         return first;
-    
-    if(first -> data <= second -> data ){
+
+    if (first->data <= second->data)
+    {
         solve(first, second);
     }
     else
     {
         solve(second, first);
     }
-    
-    
 }
